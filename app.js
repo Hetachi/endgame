30e6c5f41f3c81c517e379a38ddd9769d3190ff7
Hetachi <krikus6@inbox.lv>
2016-03-07 15:50:55 +0200

    Created posting template

--- app.js
+++ app.js
@@ -1,22 +1,53 @@
 
 var app = angular.module('firstApp', []);
 
-app.controller('UserController', function($rootScope) {
+app.service('StatusService', function(){
+  var service = this;
+  service.statuses = [];
+  var _statuses = [];
+  var _userStatuses = [];
+  service.addStatus = function(newStatus) {
+    if (!_.isEmpty(newStatus.user) && !_.isEmpty(newStatus.message)) {
+      _statuses.push(newStatus);
+
+      _userStatuses.splice(0);
+      angular.copy(_statuses, _userStatuses);
+
+    }else {
+      alert("Please define user and message");
+    }
+  }
+  service.resetStatuses = function(){
+    _statuses.splice(0);
+  }
+  service.getStatuses = function () {
+    return _statuses;
+  }
+  service.addStatuses = function(newStatus){
+    _statuses.push(newStatus);
+  };
+});
+
+app.controller('UserController', function(StatusService) {
   var vm = this;
-  vm.user = "";
-  vm.message = '';
 
   vm.setStatus = function (){
-    var _userToSend = {
+    var _newStatus = {
       user: vm.user,
       message: vm.message
     };
     console.log('Sending user status');
-    console.log(JSON.stringify(_userToSend));
-    //$rootScope.$broadcast('set-status', _userToSend);
+    console.log(JSON.stringify(_newStatus));
+    StatusService.addStatus(_newStatus);
+
   };
 });
-app.controller('StatusController', function($rootScope){
+
+app.controller('StatusController', function(StatusService){
   var vm = this;
-  vm.statuses = [];
+  vm.statuses = StatusService.getStatuses();
+
+  vm.deleteStatus = function() {
+    vm.statuses.splice(0);
+  }
 });
