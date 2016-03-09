3546ea28d29744b529480f9176e9d327be26bd0a
Hetachi <krikus6@inbox.lv>
2016-03-09 17:56:50 +0200

    Literally endgame deathsentence

--- app/app.js
+++ app/app.js
@@ -1,166 +1,209 @@
+import 'angular-material/angular-material.css';
+require('./app.css');
+import 'lodash';
+import 'angular';
+import angularMaterial from 'angular-material';
 
-var app = angular.module('firstApp', []);
 
 
-app.service('UserService', function(){
-  var service = this;
+var app = angular.module('firstApp', [
+  angularMaterial
+]);
 
-  var USERNAME_KEY = 'username';
-  var _user = {
-    username: localStorage.getItem(USERNAME_KEY),
+app.service('UserService', function () {
+    var service = this;
 
-    //TODO: Implement password support in the future
-    password: undefined
-  }
+    var USERNAME_KEY = 'username';
 
-  service.setUser = _setUser;
-  service.removeUser = _removeUser;
-  service.getUsername = _getUsername;
-  service.hasUser = _hasUser;
-
-  function _removeUser() {
-    _setUser(null);
-  }
-     function _setUser(username) {
-       _user.username = username;
-       if (_.isNull(username)){
-         localStorage.removeItem(USERNAME_KEY, username);
-       }else {
-       localStorage.setItem(USERNAME_KEY, username);
-     }
-    }
-       function _getUsername() {
-        return _user.username;
-      }
-
-        function _hasUser() {
-          // FIXME: 0 passes trough
-          return !_.isNull(_getUsername());
-        }
-          function _getUsername() {
-            return _user.username;
-          }
-});
-app.controller('LoginController', function(UserService){
-  var vm = this;
-  vm.username = '';
-  vm.login= _login;
-  function _login(){
-    UserService.setUser(vm.username);
-  }
-})
-app.service('StatusService', function($http, $timeout){
-  var service = this;
-  var SERVER_URL = 'http://10.0.1.86:8080/statuses';
-  var _statuses = [];
-  var _userStatuses = [];
-  var _users = [];
-
-  service.getStatuses = _getStatuses;
-  service.addStatus = _addStatus;
-  service.getUsers = _getUsers;
-
-  init();
-  function init(){
-    var _getRequest = {
-      method: "GET",
-      url: SERVER_URL
+    var _user = {
+        username: localStorage.getItem(USERNAME_KEY),
+        //TODO: implement password support in the future
+        password: undefined
     };
-    $http(_getRequest).then(function(res){
-      _statuses = res.data;
-      _updateUsers();
-      $timeout(init,1500);
-    });
-  };
 
+    service.setUser = _setUser;
+    service.removeUser = _removeUser;
+    service.getUsername = _getUsername;
+    service.hasUser = _hasUser;
 
-  function _addStatus(newStatus) {
-    if (!_.isEmpty(newStatus.message)) {
-    var  _postRequest = {
-        method: "POST",
-        url: SERVER_URL,
-        data: newStatus,
-      };
+    function _setUser(username) {
+        _user.username = username;
 
-      $http(_postRequest).then(function __updateStatuses(res) {
-        if (!!res) {
-          _statuses.push(res.data);
-          _updateUsers();
+        if (_.isNull(username)) {
+            localStorage.removeItem(USERNAME_KEY);
+        } else {
+            localStorage.setItem(USERNAME_KEY, username);
         }
-      });
+    }
+
+    function _removeUser() {
+        _setUser(null);
+    }
+
+    function _hasUser() {
+        return !_.isNull(_getUsername());
+    }
+
+    function _getUsername() {
+        return _user.username;
+    }
+});
+
+app.service('StatusService', function ($http, $timeout) {
+    var service = this;
+
+    var SERVER_URL = 'http://10.0.1.86:8080/statuses';
+
+    var _statuses = [];
+    var _users = {};
+
+    service.addStatus = _addStatus;
+    service.getUsers = _getUsers;
+
+    init();
+
+    function init() {
+        var _getRequest = {
+            method: 'GET',
+            url: SERVER_URL
+        };
+
+        $http(_getRequest).then(function (res) {
+            _statuses = res.data;
+
+            _updateUsers();
+
+            $timeout(init, 4000);
+        });
+    }
+
+    function _addStatus(newStatus) {
+        if (!_.isEmpty(newStatus.user) && !_.isEmpty(newStatus.message)) {
+            var _postRequest = {
+                method: 'POST',
+                url: SERVER_URL,
+                data: newStatus
+            };
+
+            $http(_postRequest).then(__updateStatuses);
+
+            function __updateStatuses(res) {
+                if (!!res) {
+                    _statuses.push(res.data);
+
+                    _updateUsers();
+                }
+            }
+        } else {
+            console.log('User and message must be defined.');
+        }
+    }
+
+    function _getUsers() {
+        return _users;
+    }
+
+    function _updateUsers() {
+        var _sortedStatuses = _.sortBy(_statuses, 'date').reverse();
+
+        var userNames = _.uniq(_.map(_statuses, 'user'));
+
+        _.each(userNames, function getUserStatus(userName) {
+            var userStatus = _.find(_sortedStatuses, function (status) {
+                return status.user === userName;
+            });
+
+            if (!!userName) {
+                if (!_users[userName]) {
+                    _users[userName] = {};
+                }
+                _users[userName].date = _.get(userStatus, 'date');
+                _users[userName].message = _.get(userStatus, 'message');
+            }
+        });
+    }
+});
+
+app.controller('MainController', function (UserService) {
+    var vm = this;
+
+    vm.hasUser = UserService.hasUser;
+    vm.getUsername = UserService.getUsername;
+    vm.removeUser = UserService.removeUser;
+});
+
+app.controller('LoginController', function (UserService) {
+    var vm = this;
+
+    vm.username = '';
+
+    vm.login = _login;
+
+    function _login() {
+        UserService.setUser(vm.username);
+    }
+});
+
+app.controller('UserController', function (UserService, StatusService) {
+    // idea of what should hapen
+    var vm = this;
+
+    _resetForm();
+
+    vm.setStatus = _setStatus;
+
+    //implementation
+
+    function _setStatus() {
+        var __newStatus = {
+            user: UserService.getUsername(),
+            message: vm.message,
+            date: new Date()
+        };
+
+        console.log('Sending user status:');
+        console.log(JSON.stringify(__newStatus));
+
+        StatusService.addStatus(__newStatus);
+
+        _resetForm();
+    }
+
+    function _resetForm() {
+        vm.message = '';
+    }
+});
+
+app.controller('StatusController', function (StatusService) {
+    var vm = this;
+
+    vm.users = StatusService.getUsers();
+});
+app.filter('orderUsers', function(){
+var lastLists = {};
+
+  return function oderUsersFilter(userMap, mapName){
+      var userList = _.map(userMap, generateNewUser);
+      var newList = _.sortBy(userList, 'date').reverse();
+
+    if (!!mapName) {
+      if (JSON.stringify(lastLists[mapName]) !== JSON.stringify(newList)) {
+        lastLists[mapName] = newList;
+      }
+      return lastLists[mapName];
     }
     else {
-      alert("Please type in message");
+      //FIXME: digest loop with no name
+      return newList;
     }
 
-  }
-
-  function _getUsers(){
-    return _users;
-  }
-
-  function _getStatuses () {
-    return _userStatuses;
-  }
-
-  function _addStatuses(newStatus){
-    _statuses.push(newStatus);
-  }
-
-  function _updateUsers(){
-    _userStatuses.splice(0);
-    var _sortedStatuses = _.sortBy(_statuses,'user')
-    angular.copy(_sortedStatuses.reverse(), _userStatuses);
-
-    _users.splice(0);
-    var userNames = _.uniq(_.map(_statuses, 'user'));
-
-    _.each(userNames, function getUserStatus(userName){
-
+    function generateNewUser(status, name) {
       var newUser = {
-        name: userName
+        name: name
       }
-      var userStatus = _.find(_userStatuses, function(status){
-        return status.user === userName;
-      })
-      if (!!userName && !!userStatus) {
-      newUser.date = userStatus.date;
-      newUser.message = userStatus.message;
-      }
-      _users.push(newUser);
-    });
+        _.defaults(newUser, status);
+        return newUser;
+    }
   }
-});
-app.controller('MainController', function(UserService){
-  var vm = this;
-  vm.hasUser = UserService.hasUser;
-  vm.getUsername = UserService.getUsername;
-  vm.removeUser = UserService.removeUser;
-});
-app.controller('UserController', function(UserService, StatusService) {
-  var vm = this;
-  _resetForm();
-    vm.setStatus = function (){
-      var _newStatus = {
-        date : vm.date,
-        user: UserService.getUsername(),
-        message: vm.message
-      };
-
-      StatusService.addStatus(_newStatus);
-      _resetForm()
-    };
-      function _resetForm() {
-        vm.date = new Date();
-        vm.message = '';
-      }
-;
-
-
-});
-
-app.controller('StatusController', function(UserService, StatusService){
-  var vm = this;
-  vm.users = StatusService.getUsers();
-  console.log(vm.statuses);
-});
+  return lastLists[mapName];
+})
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var _ = require('lodash');
 
 var cors = require('cors');
 
@@ -11,21 +12,59 @@ app.use(bodyParser.json());
 
 
 var _statuses = [];
-
+var MESSAGE_LIMIT = 100;
 app.use(express.static('app'));
 
 app.get('/statuses', function(req, res) {
   res.json(_statuses);
 });
 app.post('/statuses', function(req, res) {
-  var newStatus = req.body;
+  var IpAddress = req.connection.remoteAddress;
+  var newStatus = {
+    id: _statuses.length,
+    ip: IpAddress,
+    user: _.toString(_.get(req.body, "user")).trim(),
+    message: _parseMessage(_.get(req.body, "message")),
+    date: new Date(),
+    sendDate: new Date(_.get(req.body, "date")),
+  }
 
-  newStatus.id = _statuses.length;
+  if (_validateStatus(newStatus)) {
+    console.log('Adding new status:');
+    console.log('IP USED: ');
+    console.log(IpAddress);
+    console.log('----------------');
+    console.log(newStatus);
+    _statuses.push(newStatus);
+    res.status(201).json(newStatus);
+    console.log('----------------');
+  }
+  else {
+    res.status(404).json('Invalid message reached max MESSAGE_LIMIT');
+  }
 
-  console.log('Adding new status');
-  console.log(newStatus);
-  _statuses.push(newStatus);
-  res.status(201).json(newStatus);
+
+  function _parseMessage(message){
+    var newMessage = _.toString(message);
+    var TRUNC = "\u2026";
+
+    if(message.length > MESSAGE_LIMIT) {
+      return newMessage.substr(0, MESSAGE_LIMIT - TRUNC.length) + TRUNC;
+    }else {
+      return newMessage;
+    }
+  };
+  function _validateStatus(status){
+    var isValid = true;
+
+    isValid = isValid && !_.isEmpty(status.user);
+
+    isValid = isValid && !_.isEmpty(status.message);
+
+    isValid = isValid && !_.isNaN(status.sendDate.getTime());
+
+    return isValid;
+  }
 });
 app.listen(8080, function(){
   console.log("Server started listening on port 8080");
